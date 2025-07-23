aa31c7f67640f783c5da0ed75e8c5688d7423d14
Zhuravleva Gelena <roselifemeow@gmail.com>
2025-07-23 14:22:28 +0300

    Init commit

--- main.go
+++ main.go
@@ -2,7 +2,28 @@ package amocrmrepo
 
 import (
 	"fmt"
+	"sync"
 )
+
+type MemoryStorage struct {
+	mu sync.RWMutex
+	//posts    map[string]*model.Post
+	//comments map[string][]*model.Comment
+}
+
+// NewMemoryStorage создает новое in-memory хранилище.
+func NewMemoryStorage() *MemoryStorage {
+	return &MemoryStorage{
+		//posts:    make(map[string]*model.Post),
+		//comments: make(map[string][]*model.Comment),
+	}
+}
+
+type Accounts struct {
+}
+
+type Account_Integration struct {
+}
 
 func main() {
 	fmt.Println("git")
